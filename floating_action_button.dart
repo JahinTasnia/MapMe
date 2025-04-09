31a112f1972794363b18b759dde7641655a297b2
Jahin Tasnia <jahintasnia15@gmail.com>
2025-04-10 02:22:59 +0600

    Floating Action Button updated

---
+++ floating_action_button.dart
@@ -0,0 +1,8 @@
+floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        backgroundColor: Colors.green,
+        child: Icon(Icons.add),
+        onPressed: (){
+          MySnackbar("I'm Flaoting Action Button", context);
+        },
+      ),
