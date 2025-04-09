1f459db9503e1132ec8ad382d22898f3d744bcfb
Jahin Tasnia <jahintasnia15@gmail.com>
2025-04-09 20:13:05 +0600

    Travel places updated and commited

---
+++ travel_places.dart
@@ -0,0 +1,11 @@
+body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(height: 100,width: 100,child: Image.network("src"),),
+          Container(height: 100,width: 100,child: Image.network("src"),),
+          Container(height: 100,width: 100,child: Image.network("src"),),
+          Container(height: 100,width: 100,child: Image.network("src"),),
+          Container(height: 100,width: 100,child: Image.network("src"),),
+
+        ],
+      ),
