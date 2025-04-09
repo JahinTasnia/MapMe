e0609986d6b235762ac69b90649125faccf6c5ef
Jahin Tasnia <jahintasnia15@gmail.com>
2025-04-09 13:46:21 +0600

    Main file updated

--- main.dart
+++ main.dart
@@ -32,3 +32,38 @@ class HomeActivity extends StatelessWidget{
       SnackBar(content: Text(message))
     );
   }
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+   ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+      padding: EdgeInsets.all(20),
+      backgroundColor: Colors.green,
+      foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+   );
+
+   ButtonStyle buttonStyle2 = ElevatedButton.styleFrom(
+      minimumSize: Size(double.infinity, 60),					
+      backgroundColor: Colors.green,
+      foregroundColor: Colors.white,
+   );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Travelars"),
+        titleSpacing: 0,
+        centerTitle: true,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 0,
+        backgroundColor: Colors.lime,
+        actions: [
+          IconButton(onPressed: (){MySnackbar("search", context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){MySnackbar("comment", context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){MySnackbar("notification", context);}, icon: Icon(Icons.notifications))
+        ],
+      ),
